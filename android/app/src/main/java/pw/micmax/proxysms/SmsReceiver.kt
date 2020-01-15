2ac876a1f367547c0e821d6b47078eb16603a7fc
Michael Maxwell <mike.ian.maxwell@gmail.com>
2020-01-15 00:49:39 -0800

    Make HTTP Request with Volley

--- android/app/src/main/java/pw/micmax/proxysms/SmsReceiver.kt
+++ android/app/src/main/java/pw/micmax/proxysms/SmsReceiver.kt
@@ -4,6 +4,10 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class SmsReceiver : BroadcastReceiver() {
 
@@ -12,6 +16,24 @@ class SmsReceiver : BroadcastReceiver() {
             val phoneNumber = message.displayOriginatingAddress
             val text = message.displayMessageBody
             println("$phoneNumber: $text")
+
+            sendHTTP(context)
         }
+    }
+
+    fun sendHTTP(context: Context?) {
+        val queue = Volley.newRequestQueue(context)
+        val url = "https://www.google.com"
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response : String ->
+                println("Response is: ${response.substring(0, 500)}")
+            },
+            Response.ErrorListener {
+                println("That didn't work!")
+            }
+        )
+        
+        queue.add(stringRequest)
     }
 }
